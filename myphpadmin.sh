d0025e8415e9045c1b71d25af94cf887ab7e289b
Mr.Resor <jakusienko@gmail.com>
2024-04-26 18:31:28 +0000

    Ling up of network name.

--- myphpadmin.sh
+++ myphpadmin.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 sudo docker container run --name phpmyadmin --restart=unless-stopped \
-	-p 8081:80 --network mysql-net phpmyadmin/phpmyadmin:latest
+	-p 8081:80 --network ogame_db_net phpmyadmin/phpmyadmin:latest
