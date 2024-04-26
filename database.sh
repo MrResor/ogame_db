55a8d7614b772748864e1814585c5d7d4f29a604
Mr.Resor <jakusienko@gmail.com>
2024-04-26 18:39:44 +0000

    Ddd detach flag to docker.

--- database.sh
+++ database.sh
@@ -2,4 +2,4 @@
 
 sudo docker container run --name mysql --restart=unless-stopped \
 	-e MYSQL_ROOT_PASSWORD=$1 -p 3307:3306 \
-    -v mysql-db-vol:/var/lib/mysql --network ogame_db_net -dmysql:latest
+    -v mysql-db-vol:/var/lib/mysql --network ogame_db_net -d mysql:latest
