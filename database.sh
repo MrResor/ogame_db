9eb9afc1a4fdfb4f665d179996772b3c2dbd0001
Mr.Resor <jakusienko@gmail.com>
2024-04-28 12:17:45 +0000

    Will allow for root password to be taken from secrets.

--- database.sh
+++ database.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 sudo docker run --name mysql --restart=unless-stopped \
-	-e MYSQL_ROOT_PASSWORD=$1 -p 3307:3306 \
+	-e MYSQL_ROOT_PASSWORD="$(< ./secrets/DB_PASS.txt)" -p 3307:3306 \
     -v mysql-db-vol:/var/lib/mysql --network ogame_db_net -d mysql:latest
